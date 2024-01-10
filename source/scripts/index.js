827079348eda020ccbffdfb483ef3ffa75371535
Evgenia Isaeva <evgenia.isaeva297@gmail.com>
2024-01-10 22:47:03 +0300

    Пример слайдера

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -8,3 +8,9 @@ navToggle.addEventListener('click', () => {
   navToggle.classList.toggle('page-header__toggle--active');
   navMain.classList.toggle('page-header__nav--opened');
 });
+
+
+const slider = document.querySelector('.slider');
+document.querySelector('.slider__range').addEventListener('input', (event) => {
+  slider.style.setProperty('--slide-width', `${event.target.value}%`);
+});
